452728584d6c6e9a35798d783ffda8774e18ae80
Luis Felipe Silva De Vidts <felipe.silva@unholster.com>
2022-10-13 21:10:25 -0300

    Add install

---
+++ .install/macos.sh
@@ -0,0 +1,23 @@
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install minimum brew formulae
+brew install pyenv
+brew install pyenv-virtualenv
+brew install rbenv ruby-build
+brew install nvm
+brew install postgresql
+
+brew install vim
+brew install gh
+brew install pdm
+# FZF
+brew install fzf
+brew install bat
+brew install the_silver_searcher
+brew install ripgrep
+brew install git-delta
+
+# Install tmuxinator
+brew install tmux
+
+
